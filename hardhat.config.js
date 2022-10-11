82f1b08c7e4d42b33fa64999f9347a4d87d39a98
Malek <malekmaher100@gmail.com>
2022-10-11 13:32:25 +0300

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,56 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337
+        }
+    },
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }]
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0
+        }
+    }
+}
+
+task("accounts", "Prints the list of accounts", async () => {
+    const accounts = await ethers.getSigners()
+
+    for (const account of accounts) {
+        console.log(account.address)
+    }
+})
